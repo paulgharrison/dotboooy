15adb15254a41ccfc065172fd6e27491a769213d
paulgharrison <pg.harrison@gmail.com>
2014-12-18 21:08:02 -0500

    Temp Release Changes
    Temporary changes to make the track release happen, plus google
    analytics.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
 	$("#logoDiv").delay(1500).fadeIn(4000);
+	$('#bgimg0').delay(500).fadeOut(3000)
 	$('#bgimg1').delay(500).fadeIn(3000).foggy({
-		blurRadius: 5,
+		blurRadius: 10,
 		opacity: 1
 	});
+	$("#scPlayer").delay(3000).fadeIn(4000);
 });
